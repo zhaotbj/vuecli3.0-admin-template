db7558ef6d2af061cf51838c0894369a5bb05164
zhaotbj <975619483@qq.com>
2019-05-01 15:38:04 +0800

    vuecli3.0初始化

---
+++ README.md
@@ -0,0 +1,39 @@
+# project1
+
+```
+npm run dev 调用本地环境api
+npm run test 调用测试环境api
+npm run build 调用线上环境api
+```
+
+
+
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-container>
+    <el-header>
+      Header
+      <el-button @click="handlebtn">btn</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu class="el-menu-vertical-demo" default-active="2" :unique-opened="false" router>
+          <el-menu-item index="module1">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航一</span>
+          </el-menu-item>
+          <el-menu-item index="module2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="module3">
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="module4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+
+import axios from 'axios'
+export default {
+  name: "home",
+  components: {},
+  methods: {
+    handlebtn(){
+      this.$request.post('/apply/selectApplyTarget').then(res=>{
+        console.log(res)
+      })
+    }
+  }
+};
+</script>
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
